12c94ef34c0dd015a7145896758d0ae27e7e5966
Matthias Gilch <matthias.gilch.mg@gmail.com>
2022-09-25 20:22:38 +0200

    Had to go for f64 because of lost precision
    The overall number of pixels over the whole map
    can be calculated as the following:
    
    given zoom level: z
    number of tiles in one direction: n
    n = 2^z
    n_pixels = n * 256
    => n_pixels = 2^(z+8)
    
    This means to support the full map with integer
    numbers we need log2(2^(z+8)) bits
    f32 mantisse is 23bits => working until zoom 15
    f64 mantisse is 52bits => working until zoom 44

--- lib/tile.rs
+++ lib/tile.rs
@@ -13,42 +13,50 @@ impl Tile {
     }
 
     pub fn from_coord(coord: Coord, zoom: i32) -> Self {
-        let pi = std::f32::consts::PI;
-        let n: f32 = 2f32.powi(zoom);
+        let pi = std::f64::consts::PI;
+        let n: f64 = 2f64.powi(zoom);
         println!("Z{:<2} -> n={n}", zoom);
 
-        let lat_rad = coord.lat().to_radians();
-        let lon_rad = coord.lon().to_radians();
+        let lat_rad = coord.lat().to_radians() as f64;
+        let lon_rad = coord.lon().to_radians() as f64;
 
-        let x = (((lon_rad + pi) / (2f32 * pi)) * n).floor() as i32;
+        let x = (((lon_rad + pi) / (2f64 * pi)) * n).floor() as i32;
         let y = {
             let asinh = lat_rad.tan().asinh() / pi;
-            (1f32 - asinh) * (n / 2f32)
+            (1f64 - asinh) * (n / 2f64)
         }
         .floor() as i32;
 
         Self::new(x, y, zoom)
     }
 
-    pub fn origin_pixel(&self) -> (f32, f32) {
-        let x: f32 = self.x as f32 * 256f32;
-        let y: f32 = self.y as f32 * 256f32;
+    pub fn contains(&self, coord: &Coord) -> bool {
+        let (mpx, mpy) = self.map_pixel(coord);
+        let x_bounds = (0f64..256f64).contains(&mpx);
+        let y_bounds = (0f64..256f64).contains(&mpy);
+        x_bounds && y_bounds
+    }
+
+    pub fn origin_pixel(&self) -> (f64, f64) {
+        let x: f64 = self.x as f64 * 256f64;
+        let y: f64 = self.y as f64 * 256f64;
         (x, y)
     }
 
-    pub fn pixel(&self, coord: &Coord) -> (f32, f32) {
+    pub fn pixel(&self, coord: &Coord) -> (f64, f64) {
         let (x, y) = self.map_pixel(coord);
-        (x, y % 256f32)
+        let (x_orig, y_orig) = self.origin_pixel();
+        (x - x_orig, y - y_orig)
     }
 
-    pub fn map_pixel(&self, coord: &Coord) -> (f32, f32) {
-        let pi = std::f32::consts::PI;
-        let lat_rad = coord.lat().to_radians();
-        let lon_rad = coord.lon().to_radians();
-        let n = 2f32.powi(self.zoom);
-        let common = (256f32 / (2f32 * pi)) * n;
+    pub fn map_pixel(&self, coord: &Coord) -> (f64, f64) {
+        let pi = std::f64::consts::PI;
+        let lat_rad = coord.lat().to_radians() as f64;
+        let lon_rad = coord.lon().to_radians() as f64;
+        let n = 2f64.powi(self.zoom);
+        let common = (256f64 / (2f64 * pi)) * n;
         let x = common * (lon_rad + pi);
-        let y = common * (pi - (pi / 4f32 + lat_rad / 2f32).tan().ln());
+        let y = common * (pi - (pi / 4f64 + lat_rad / 2f64).tan().ln());
         (x, y)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,1 @@
-use mapgen::{Coord, Tile};
-
-fn main() {
-    let c = Coord::new(-0.1f32, 5f32);
-    let tile = Tile::from_coord(c, 5);
-    println!("{tile:?} <- {c:?}");
-    println!("{:?}", tile.pixel(&c));
-}
+fn main() {}
